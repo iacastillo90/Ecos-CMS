2e23de2e6f10278e747f814ce0ee1f0d46f4fa7a
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2025-11-12 20:18:59 -0300

    first commit

---
+++ Backend/src/main/java/com/edtech/Ecos_cms/EcosCmsApplication.java
@@ -0,0 +1,13 @@
+package com.edtech.Ecos_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosCmsApplication.class, args);
+	}
+
+}
